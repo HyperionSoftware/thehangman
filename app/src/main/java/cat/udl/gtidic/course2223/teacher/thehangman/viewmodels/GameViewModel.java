7c3fd694c9d9676501a790af2ce1ef02ac00b166
JoanBonell <joanbonellruiz@gmail.com>
2023-06-05 18:17:45 +0200

    Requisit 2: MVVM with dataBinding and LiveData

--- app/src/main/java/cat/udl/gtidic/course2223/teacher/thehangman/viewmodels/GameViewModel.java
+++ app/src/main/java/cat/udl/gtidic/course2223/teacher/thehangman/viewmodels/GameViewModel.java
@@ -1,7 +1,28 @@
 package cat.udl.gtidic.course2223.teacher.thehangman.viewmodels;
 
+import androidx.lifecycle.LiveData;
+import androidx.lifecycle.MutableLiveData;
 import androidx.lifecycle.ViewModel;
 
-public class GameViewModel extends ViewModel {
+import cat.udl.gtidic.course2223.teacher.thehangman.models.Game;
 
+public class GameViewModel extends ViewModel {
+    Game game;
+    private final MutableLiveData<String> visibleWord = new MutableLiveData<>();
+    public LiveData<String> getVisibleWord() {
+        return visibleWord;
+    }
+    public Game getGame() {
+        return game;
+    }
+
+    public void startGame(){
+        game = new Game();
+    }
+
+    public int addLetter(String novaLletra) {
+        int value = game.addLetter(novaLletra);
+        visibleWord.setValue(game.visibleWord());
+        return value;
+    }
 }
